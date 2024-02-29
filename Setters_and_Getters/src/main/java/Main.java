180b4f46b3d53124af62add2b677e721bfeabd55
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2024-03-01 00:48:02 +0100

    Update Main.java

--- Setters_and_Getters/src/main/java/Main.java
+++ Setters_and_Getters/src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = Person("Jose", 19,  true, 0);
+        Person person2 = Person("Maria", 41,  false, 1);
     }
 }

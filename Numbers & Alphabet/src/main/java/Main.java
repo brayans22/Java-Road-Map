4e3084c5bfa5cf4aa9ce75e299f5954750d7969d
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2024-03-01 01:56:24 +0100

    Delete Numbers & Alphabet directory

--- Numbers & Alphabet/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ThreadCase threadNumber = new ThreadCase(1);
-        ThreadCase threadAlphabet = new ThreadCase(2);
-
-        threadNumber.run();
-        threadAlphabet.run();
-    }
-}
